936b541de1df73c4f76e8fadcaa6924671ab27c4
Mauricio Gonzalez <migonzalez@fi.uba.ar>
2023-02-10 01:08:33 -0300

    estructura del proyecto modificada, users router

--- index.js
+++
@@ -1,7 +0,0 @@
-import startApi, { initializeProducts } from "./src/server/rest-api.js";
-
-// funcion creada solo para que el corrector teste el funcionamiento
-initializeProducts();
-
-
-startApi();
---
+++ src/app.js
@@ -0,0 +1,20 @@
+import Express from 'express';
+import UsersRouter from './routes/users.js'
+const PORT = 8080;
+const app = Express();
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`API RUNNING ON PORT: ${PORT}`);
+});
+
+app.get('/', (_, res) => {
+    const message = `Bienvenido a mi rest api`;
+    res.status(200).json({
+        ok: true,
+        message: message,
+        products: {}
+    });
+});
+app.use('/api/products', UsersRouter);
