fa9a7fd866257f8be29f99a0bee71b8982f21999
Mauricio Gonzalez <migonzalez@fi.uba.ar>
2023-05-26 13:03:49 -0300

    policies premium and create/modify product for premium user

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -16,10 +16,10 @@ class ViewsRoutes {
 
   initViewsRoutes() {
     this.router.param('cid', this.controller.validateCIDParam)
-    this.router.get(`${this.path}carts/:cid`,authMdw, handlePolicies([policies.USER, policies.ADMIN]), this.controller.getCart);
-    this.router.get(`${this.path}products`, authMdw, handlePolicies([policies.USER, policies.ADMIN]), this.controller.getProducts);
-    this.router.get('/realtimeproducts',authMdw, handlePolicies([policies.USER]), this.controller.getRealTimeProducts);
-    this.router.get('/chat',authMdw, handlePolicies([policies.USER]),this.controller.getChat);
+    this.router.get(`${this.path}carts/:cid`,authMdw, handlePolicies([policies.USER,policies.PREMIUM, policies.ADMIN]), this.controller.getCart);
+    this.router.get(`${this.path}products`, authMdw, handlePolicies([policies.USER,policies.PREMIUM, policies.ADMIN]), this.controller.getProducts);
+    this.router.get('/realtimeproducts',authMdw, handlePolicies([policies.USER,policies.PREMIUM]), this.controller.getRealTimeProducts);
+    this.router.get('/chat',authMdw, handlePolicies([policies.USER,policies.PREMIUM]),this.controller.getChat);
     this.router.get('/login', handlePolicies([policies.PUBLIC]), this.controller.getLogin);
     this.router.get("/register", handlePolicies([policies.PUBLIC]), this.controller.getRegister);
     this.router.get("/faillogin", handlePolicies([policies.PUBLIC]), this.controller.getFailLogin);
