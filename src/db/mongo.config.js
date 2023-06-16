515d1383635aea0a07b2daa761f1ca79d4b136d2
mivagonzalez <gonza.mauricioivan@gmail.com>
2023-06-16 16:54:03 -0300

    mongo db use local database

--- src/db/mongo.config.js
+++ src/db/mongo.config.js
@@ -3,9 +3,8 @@ const { connect } = require("mongoose");
 const { DB_USE_ATLAS, DB_USER, DB_NAME, DB_PASSWORD, LOCAL_DB_NAME, LOCAL_DB_HOST, LOCAL_DB_PORT } = require('../config/config')
 const LOCAL_MONGO_CONNECTION=`mongodb://${LOCAL_DB_HOST}:${LOCAL_DB_PORT}/${LOCAL_DB_NAME}`;
 const WEB_MONGO_CONNECTION = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_NAME}.aqkxapv.mongodb.net/${DB_NAME}`
-const TEST_MONGO_CONNECTION = 'mongodb://127.0.0.1:27017/ecommerce-test'
 const configConnection = {
-  url: DB_USE_ATLAS === "true" ? WEB_MONGO_CONNECTION : TEST_MONGO_CONNECTION,
+  url: DB_USE_ATLAS === "true" ? WEB_MONGO_CONNECTION : LOCAL_MONGO_CONNECTION,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
